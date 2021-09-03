07949ba7cb96cca9081f9136cdf88f7531a69850
wylovecy <915333806@qq.com>
2021-09-03 20:18:06 +0800

    123456

---
+++ out/artifacts/javaweb01_war_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2021/9/2
+  Time: 11:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>I have no word!!!</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
---
+++ src/helloworld.java
@@ -0,0 +1,22 @@
+
+public class helloworld
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("helloworld!!");
+        for (int i = 0; i <10 ; i++)
+        {
+            System.out.println(i);
+        }
+        char a=100;
+        if (a == 1)
+        {
+            System.out.println("1111111111111111");
+
+        }
+
+
+    }
+
+}
